3b0d96dc4c30fdd9ce75e3d57de8bab92ce89796
Andrew Darqui <andrew.darqui@gmail.com>
2015-03-08 21:19:17 -0400

    Timeit instead of TimeIt

--- examples/simple/time_stuff.go
+++ examples/simple/time_stuff.go
@@ -1,34 +1,29 @@
 package main
 
 import (
-    "github.com/adarqui/timeit-go"
-    "time"
+	"github.com/adarqui/timeit-go"
+	"time"
 )
-
 
 func nothingFunc() {
 }
 
-
 func loopFunc() {
-    for i := 1; i < 1000000000; i++ {
-    }
+	for i := 1; i < 1000000000; i++ {
+	}
 }
-
 
 func sleepFunc() {
-    time.Sleep(5 * time.Second)
+	time.Sleep(5 * time.Second)
 }
-
 
 func possibleError() (interface{}, interface{}) {
-    return true, nil
+	return true, nil
 }
 
-
 func main() {
-    timeit.TimeItPrint(nothingFunc)
-    timeit.TimeItPrint(loopFunc)
-    timeit.TimeItPrint(sleepFunc)
-    timeit.TimeIt2Print(possibleError)
+	timeit.TimeitPrint(nothingFunc)
+	timeit.TimeitPrint(loopFunc)
+	timeit.TimeitPrint(sleepFunc)
+	timeit.Timeit2Print(possibleError)
 }
--- timeit.go
+++ timeit.go
@@ -2,11 +2,10 @@
 package timeit
 
 import (
-    "syscall"
-    "time"
-    "fmt"
+	"fmt"
+	"syscall"
+	"time"
 )
-
 
 // Time spent:
 // - Elapsed : total elapsed time during execution of the function.
@@ -14,84 +13,75 @@ import (
 // - Sys : time spent in sys space during execution of the function.
 // - Ticks : number of cpu ticks that occurred during execution of the function.
 type TimeSpent struct {
-    Elapsed time.Duration
-    User int64
-    Sys int64
-    Ticks int64
+	Elapsed time.Duration
+	User    int64
+	Sys     int64
+	Ticks   int64
 }
-
 
 func logResult(ts *TimeSpent) string {
-    return fmt.Sprintf("TimeIt: Elapsed=%s, User=%dus, Sys=%dus, Ticks=%d", ts.Elapsed.String(), ts.User, ts.Sys, ts.Ticks)
+	return fmt.Sprintf("Timeit: Elapsed=%s, User=%dus, Sys=%dus, Ticks=%d", ts.Elapsed.String(), ts.User, ts.Sys, ts.Ticks)
 }
-
 
 // Simple wrapper. Returns a string.
-func TimeIt(cb func()) string {
-    cbW := func() interface{} { cb(); return true }
-    ts, _ := TimeItT(cbW)
-    return logResult(ts)
+func Timeit(cb func()) string {
+	cbW := func() interface{} { cb(); return true }
+	ts, _ := TimeitT(cbW)
+	return logResult(ts)
 }
-
 
 // Simple wrapper. Returns a string.
-func TimeIt1(cb func() interface{}) string {
-    ts, _ := TimeItT(cb)
-    return logResult(ts)
+func Timeit1(cb func() interface{}) string {
+	ts, _ := TimeitT(cb)
+	return logResult(ts)
 }
-
 
 // Simple wrapper. Returns a string.
-func TimeIt2(cb func() (interface{}, interface{})) string {
-    ts, _, _ := TimeItT2(cb)
-    return logResult(ts)
+func Timeit2(cb func() (interface{}, interface{})) string {
+	ts, _, _ := TimeitT2(cb)
+	return logResult(ts)
 }
-
 
 // Simple wrapper. Prints a summary string.
-func TimeItPrint(cb func()) {
-    fmt.Println(TimeIt(cb))
+func TimeitPrint(cb func()) {
+	fmt.Println(Timeit(cb))
 }
-
 
 // Simple wrapper. Prints a summary string.
-func TimeIt1Print(cb func() interface{}) {
-    fmt.Println(TimeIt1(cb))
+func Timeit1Print(cb func() interface{}) {
+	fmt.Println(Timeit1(cb))
 }
-
 
 // Simple wrapper. Prints a summary string.
-func TimeIt2Print(cb func() (interface{}, interface{})) {
-    fmt.Println(TimeIt2(cb))
+func Timeit2Print(cb func() (interface{}, interface{})) {
+	fmt.Println(Timeit2(cb))
 }
-
 
 // Measures the pre/post timing information to execute cb(). Returns the results in a TimeSpent struct.
 // Used with function of one return value.
-func TimeItT(cb func() interface{}) (*TimeSpent, interface{}) {
-    cbW := func() (interface{}, interface{}) { r1 := cb(); return r1, true }
-    ts, r1, _ := TimeItT2(cbW)
-    return ts, r1
+func TimeitT(cb func() interface{}) (*TimeSpent, interface{}) {
+	cbW := func() (interface{}, interface{}) { r1 := cb(); return r1, true }
+	ts, r1, _ := TimeitT2(cbW)
+	return ts, r1
 }
-
 
 // Measures the pre/post timing information to execute cb(). Returns the results in a TimeSpent struct.
 // Used with functions of two return values.
-func TimeItT2(cb func() (interface{}, interface{})) (*TimeSpent, interface{}, interface{}) {
-    ts := new(TimeSpent)
-    tmsPre := new(syscall.Tms)
-    tmsPost := new(syscall.Tms)
+func TimeitT2(cb func() (interface{}, interface{})) (*TimeSpent, interface{}, interface{}) {
+	ts := new(TimeSpent)
+	tmsPre := new(syscall.Tms)
+	tmsPost := new(syscall.Tms)
 
-    timePre := time.Now()
-    ticksPre, _ := syscall.Times(tmsPre)
-    r1, r2 := cb()
-    ticksPost, _ := syscall.Times(tmsPost)
-    timePost := time.Now()
+	timePre := time.Now()
+	ticksPre, _ := syscall.Times(tmsPre)
+	r1, r2 := cb()
+	ticksPost, _ := syscall.Times(tmsPost)
+	timePost := time.Now()
 
-    ts.Elapsed = timePost.Sub(timePre)
-    ts.User = tmsPost.Utime - tmsPre.Utime
-    ts.Sys = tmsPost.Stime - tmsPre.Stime
-    ts.Ticks = int64(ticksPost - ticksPre)
+	ts.Elapsed = timePost.Sub(timePre)
+	ts.User = tmsPost.Utime - tmsPre.Utime
+	ts.Sys = tmsPost.Stime - tmsPre.Stime
+	ts.Ticks = int64(ticksPost - ticksPre)
 
-    return ts, r1, r2
+	return ts, r1, r2
 }
